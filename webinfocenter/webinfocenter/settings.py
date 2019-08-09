bd39a45fff46974fe12790b3ee9190953f69fd67
Mark <markcyl@gmail.com>
2019-08-09 16:46:06 +0800

    using postgres database.

--- webinfocenter/webinfocenter/settings.py
+++ webinfocenter/webinfocenter/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'webinfocenter.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'changeme',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
